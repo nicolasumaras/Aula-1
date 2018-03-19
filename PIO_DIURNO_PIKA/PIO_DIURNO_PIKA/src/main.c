ca7c8694d289f83d2838475acbecaf32c7bda258
Aluno <Aluno>
2018-03-19 20:04:53 -0300

    todas as leds piscando

--- PIO_DIURNO_PIKA/PIO_DIURNO_PIKA/src/main.c
+++ PIO_DIURNO_PIKA/PIO_DIURNO_PIKA/src/main.c
@@ -31,6 +31,7 @@
 #include <asf.h>
 #define PA19 (1<<19)
 #define PA20 (1<<20)
+#define PC20 (1<<20)
 
 int main (void)
 {
@@ -42,19 +43,30 @@ int main (void)
 
 
 
-
-	PIOA->PIO_PER |= (PA19 | PA20); //chama o registrador certo e escreve o bit 1 deslocando o 1 19 posições para o ponto certo do bus
+		PIOA->PIO_PER |= (PA19 | PA20); //chama o registrador certo e escreve o bit 1 deslocando o 1 19 posições para o ponto certo do bus
+		PIOC->PIO_PER |= (PC20);
 	//pode ser feito PIOA->PIO_PER = (1<<19); | (1<<20) realizando uma operação ou resultando em 001100000000000
-	PIOA->PIO_OER |= (PA19) | (PA20);
-
+		PIOA->PIO_OER |= (PA19 | PA20);
+		PIOC->PIO_OER |= (PC20);
+		PIOA->PIO_SODR |= PA20; //acende o LED 20
+		PIOA->PIO_SODR |= PA19; //apaga o LED 19
+		PIOC->PIO_CODR |= PC20;
 
 
 
 	while (1){
-		PIOA->PIO_CODR |= PA20;
-		PIOA->PIO_SODR |= PA19;
+		PIOA->PIO_CODR |= PA20; //acende o LED 20
+		PIOA->PIO_SODR |= PA19; //apaga o LED 19
+		PIOC->PIO_CODR |= PC20;
+		delay_s(1);
+		PIOC->PIO_CODR |= PC20;
 		PIOA->PIO_CODR |= PA19;
-		PIOA->PIO_SODR |= PA20;
-
+		PIOA->PIO_SODR |= PA20; //Apaga o LED 20
+		delay_s(1);
+		PIOA->PIO_SODR |= PA20; //acende o LED 20
+		PIOA->PIO_SODR |= PA19; //apaga o LED 19
+		PIOC->PIO_SODR |= PC20;
+		
+		delay_s(1);
 	}
 }
